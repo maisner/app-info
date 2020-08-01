962d6bb43f33070c9bdc91d944f910d45cf1911d
Michal Šmeidler <smeidler.michal@gmail.com>
2020-08-02 01:17:03 +0200

    Sort sql queries by time

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
-  mainWindow.setMenu(null);
+  // mainWindow.setMenu(null);
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();

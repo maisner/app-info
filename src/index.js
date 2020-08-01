f31b0728b2f4e741ecbeb69938a10d986c799da0
Michal Šmeidler <smeidler.michal@gmail.com>
2020-08-02 00:57:49 +0200

    Update UI

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ const createWindow = () => {
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.setMenu(null);
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
